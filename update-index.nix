07a1991f7a9055061b6e0b896ee3a6cb478472aa
funketh <45576110+funketh@users.noreply.github.com>
2021-11-02 15:41:34 +0100

    quote variable expansions

--- update-index.nix
+++ update-index.nix
@@ -17,12 +17,12 @@ writeScript "update-index" ''
 
   # on flake based installations nixpkgs is specified via
   # flake input and therefore NIX_PATH might be unset
-  if echo $NIX_PATH | grep -q "nixpkgs="; then
+  if echo "$NIX_PATH" | grep -q "nixpkgs="; then
     nixpkgs=""
   else
     nixpkgs="-I nixpkgs=${pkgs.path}"
   fi
 
-  mkdir -p $HOME/.cache/comma/
-  nix-index -d $HOME/.cache/comma/index -f $nixpkgs
+  mkdir -p "$HOME/.cache/comma/"
+  nix-index -d "$HOME/.cache/comma/index" -f "$nixpkgs"
 ''
